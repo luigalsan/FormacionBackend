84b94b4b700617162a8761b625271c0040dd56ac
Luis Miguel <luismiguel.gallego@bosonit.com>
2023-08-10 14:56:32 +0200

    poniendo ruta relativa

--- block1-process-file-and-streams/src/main/java/org/example/Main.java
+++ block1-process-file-and-streams/src/main/java/org/example/Main.java
@@ -94,7 +94,7 @@ public class Main {
     public static void main(String[] args) {
         try {
 
-            //TENGO QUE PONER RUTA ABSOLUTA PORQUE CUANDO CREO MODULO NO ES CAPAZ DE ENCONTRAR EL .csv EN RUTA RELATIVA
+
             List<Person> people = readPeopleFromFile("src/main/resources/people.csv");
 
 
