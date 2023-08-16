673569c7273ba464d99c8fa87419077b1705e4a4
Luis Miguel <luismiguel.gallego@bosonit.com>
2023-08-16 16:47:08 +0200

    subiendo ejercicios profile

---
+++ block5-profiles/src/main/java/com/example/block5profiles/ProfileRunner.java
@@ -0,0 +1,22 @@
+package com.example.block5profiles;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfileRunner implements CommandLineRunner {
+
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Value("${bd.url}")
+    private String dbUrl;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Active Profile: " + activeProfile);
+        System.out.println("DB URL: " + dbUrl);
+    }
+}
