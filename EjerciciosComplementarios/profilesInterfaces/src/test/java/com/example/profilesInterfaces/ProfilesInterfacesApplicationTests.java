92ed8bc794d735041f7fdabf022ba75ad922f68e
Luis Miguel <luismiguel.gallego@bosonit.com>
2023-08-21 10:44:27 +0200

    subiendo EjercicioComplementario profilesInterfaces

---
+++ EjerciciosComplementarios/profilesInterfaces/src/test/java/com/example/profilesInterfaces/ProfilesInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profilesInterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesInterfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
