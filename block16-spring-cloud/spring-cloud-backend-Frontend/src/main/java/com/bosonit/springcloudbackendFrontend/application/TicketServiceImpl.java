f2bdffbf2d6a09d3651067015b3c9848d06b8ad9
Luis Miguel <luismiguel.gallego@bosonit.com>
2023-10-25 13:44:36 +0200

    subiendo la aplicación con base de datos H2

--- block16-spring-cloud/spring-cloud-backend-Frontend/src/main/java/com/bosonit/springcloudbackendFrontend/application/TicketServiceImpl.java
+++ block16-spring-cloud/spring-cloud-backend-Frontend/src/main/java/com/bosonit/springcloudbackendFrontend/application/TicketServiceImpl.java
@@ -20,16 +20,19 @@ public class TicketServiceImpl implements TicketService {
     TicketRepository ticketRepository;
     @Override
     public TicketOutputDto generateTicket(int userId, int viajeId) {
+
         String urlCliente = "http://localhost:8081/cliente/" + userId;
         String urlViaje = "http://localhost:8081/viaje/" + viajeId;
+
         Cliente cliente = restTemplate.getForObject(urlCliente, Cliente.class);
         Viaje viaje = restTemplate.getForObject(urlViaje, Viaje.class);
+
         if (cliente != null && viaje != null){
             TicketInputDto ticketInputDto = new TicketInputDto(cliente.getId(),cliente.getNombre(),cliente.getApellido(),cliente.getEmail(),viaje.getOrigin(),viaje.getDestination(),
                     viaje.getDepartureDate(),viaje.getArrivalDate());
             return ticketRepository.save(new Ticket(ticketInputDto)).ticketInputToOutputDto();
         } else {
-            throw new RuntimeException("Error al encontrar al cliente o el viaje");
+            throw new RuntimeException("No existe el cliente ni el viaje");
         }
 
 
